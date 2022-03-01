9c1b863ec2e22a7e86db694e801b864a9b568129
Mahathir Rojan <torbengalifreind@gmail.com>
2022-03-01 11:53:02 -0500

    updated READme and added the first func

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 #include "funcs.h"
+#include <string> 
 
 // add functions here
 
+bool isDivisibleBy(int n, int d){
+    std::string result = ""; 
+    if(d == 0){
+        return false; 
+    }
+
+
+    if(n % d == 0){
+        return true; 
+    }else{
+        return false; 
+    }
+}
+
+bool isPrime(int n){
+    
+}
